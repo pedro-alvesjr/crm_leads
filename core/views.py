01ca974d6f4e0d1fe9a72820b2e97370c2d3512b
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-10-03 06:17:56 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import models
 from .models import Lead
 from django import forms
 
 # Dashboard
 def dashboard(request):
-    contagem = Lead.objects.values_list('status').order_by('status').count()
     contagem = Lead.objects.values('status').order_by('status').distinct().annotate(count=models.Count('status'))
     return render(request, 'dashboard.html', {'contagem': {c['status']: c['count'] for c in contagem}})
 
@@ -14,18 +13,19 @@ def lista_leads(request):
     leads = Lead.objects.all()
     return render(request, 'lista_leads.html', {'leads': leads})
 
-# Criar nova lead
+# Formulário para criar Lead
 class LeadForm(forms.ModelForm):
     class Meta:
         model = Lead
         fields = ['nome', 'email', 'telefone', 'status', 'parceiro']
 
+# Criar nova lead
 def criar_lead(request):
     if request.method == "POST":
         form = LeadForm(request.POST)
         if form.is_valid():
             form.save()
-            return lista_leads(request)
+            return redirect('lista_leads')  # Melhor prática
     else:
         form = LeadForm()
     return render(request, 'criar_lead.html', {'form': form})
